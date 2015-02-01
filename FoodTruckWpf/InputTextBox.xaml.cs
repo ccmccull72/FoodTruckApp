9f11599f9b47c268beba73415f4be08fc059874a
ccmccull <ccmccull@gmail.com>
2015-02-01 11:54:56 -0800

    second implementation

---
+++ FoodTruckWpf/InputTextBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodTruckWpf
+{
+   /// <summary>
+   /// Interaction logic for InputTextBox.xaml
+   /// </summary>
+   public partial class InputTextBox : Window
+   {
+      public string inputText = "";
+
+      public InputTextBox(string message)
+      {
+         InitializeComponent();
+         messageLabel.Content = message;
+         truckNameTextBox.Focus();
+      }
+
+      private void okButton_Click(object sender, RoutedEventArgs e)
+      {
+         inputText = truckNameTextBox.Text;
+         Close();
+      }
+
+      private void cancelButton_Click(object sender, RoutedEventArgs e)
+      {
+         Close();
+      }
+   }
+}
